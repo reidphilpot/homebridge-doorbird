1a18ce5616587227aaa177be1034cf0aba34b4ee
brownad <adrian-brown@mail.com>
2017-01-13 17:29:12 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,67 +1,95 @@
 var Service, Characteristic;
+var requst = require('superagent');
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerPlatform("homebridge-doorbell", "Doorbell", DoorbellPlatform);
+  homebridge.registerPlatform("homebridge-doorbird", "Doorbell", DoorBirdPlatform);
 }
 
-function DoorbellPlatform(log, config) {
+function DoorBirdPlatform(log, config) {
   // global vars
   this.log = log;
-  
+
   // configuration vars
   this.devices = config["doorbells"];
-  
+
   log("Starting discovery...");
 }
 
-DoorbellPlatform.prototype = {
+DoorBirdPlatform.prototype = {
   accessories: function(callback) {
     var foundAccessories = [];
     var count = this.devices.length;
-    
+
     for(index = 0; index < count; index++){
-		  var accessory  = new DoorbellAccessory(this.log, this.devices[index]);
+		  var accessory  = new DoorBirdAccessory(this.log, this.devices[index]);
 		  foundAccessories.push(accessory);
 	  }
-	
+
 	  callback(foundAccessories);
   }
 }
 
-function DoorbellAccessory(log, config) {
+function DoorBirdAccessory(log, config) {
   this.log = log;
   this.name = config["name"];
-  this.doorbellName = config["doorbell_name"] || this.name; // fallback to "name" if you didn't specify an exact "button_name"
+  this.username = config["username"];
+  this.password = config["password"];
+  this.doorbird_ip = config["doorbird_ip"];
+  this.check_request = config["check_request"];
+  this.url = "http://" + this.username + ":" + this.password + "@" + this.doorbird_ip + this.check_request
   this.binaryState = 0; // switch state, default is OFF
-  this.log("Starting a homebridge-doorbell device with name '" + this.doorbellName + "'...");
+  this.log("Starting a homebridge-doorbird device with name '" + this.name + "'...");
   this.service;
-  this.timeout = 2; // Timeout in seconds
+  this.timeout = 2;
+  this.lastState = 0
 }
 
-DoorbellAccessory.prototype.getState = function(callback) {
+DoorBirdAccessory.prototype.request = function() {
+  // make request
+  request
+    .get(this.url)
+    .end(function(err, res){
+      if(err || !res.ok) {
+        this.log('DoorBird connection error: ' + err)
+      } else {
+          this.log('DoorBird response: ' + res.text);
+          if (this.lastState !== res.thisState) {
+          }
+          this.lastState = res.thisState
+        });
+    }
+
+  setTimeout(function() {
+      this.log("Doorbell pressed");
+      this.service.getCharacteristic(Characteristic.ProgrammableSwitchEvent).setValue(res.thisState);
+  }.bind(this), 10000);
+}
+
+DoorBirdAccessory.prototype.getState = function(callback) {
   var powerOn = this.binaryState > 0;
-  this.log("Power state for the '%s' is %s", this.doorbellName, this.binaryState);
+  this.log("Power state for the '%s' is %s", this.name, this.binaryState);
   callback(null, powerOn);
 }
 
-DoorbellAccessory.prototype.setPowerOn = function(powerOn, callback) {
+DoorBirdAccessory.prototype.setPowerOn = function(powerOn, callback) {
   var self = this;
   this.binaryState = powerOn ? 1 : 0;
-  this.log("Set power state on the '%s' to %s", this.doorbellName, this.binaryState);
+  this.log("Set power state on the '%s' to %s", this.name, this.binaryState);
   callback(null);
 
   if(powerOn) {
     setTimeout(function() {
       self.log("BEEP! BOOP!");
       self.service.getCharacteristic(Characteristic.On).setValue(0);
+      setInterval(self.request.bind(self), 100)
     }, this.timeout * 1000);
   }
 }
 
 
-DoorbellAccessory.prototype.identify = function(callback) {
+DoorBirdAccessory.prototype.identify = function(callback) {
     this.log("Identify requested!");
 
     var targetChar = this.service
@@ -70,33 +98,26 @@ DoorbellAccessory.prototype.identify = function(callback) {
     this.log("targetChar:", targetChar);
 
     if (targetChar.value == "1"){
-	targetChar.setValue(0);
-	this.log("Toggle state to 0");
+	     targetChar.setValue(0);
+	     this.log("Toggle state to 0");
     }
     else{
-	targetChar.setValue(1);
-	this.log("Toggle state to 1");
+	     targetChar.setValue(1);
+	     this.log("Toggle state to 1");
     }
-    callback(); // success
+    callback();
 }
 
-DoorbellAccessory.prototype.getServices = function() {
-    // register the service and provide the functions
+DoorBirdAccessory.prototype.getServices = function() {
+
     this.service = new Service.Doorbell(this.name);
 
     this.service
       .getCharacteristic(Characteristic.ProgrammableSwitchEvent)
       .on('get', this.getState.bind(this));
-    
+
     var targetChar = this.service
     .getCharacteristic(Characteristic.ProgrammableSwitchEvent);
-
-    // DBG: Fire an event 10s after start 
-    setTimeout(function() {
-        this.log("Ding Dong");
-        this.service.getCharacteristic(Characteristic.ProgrammableSwitchEvent).setValue(1);
-    }.bind(this), 10000);
-
 
 return [this.service];
 }
