b4fe41cd68af780f897b068af658ba9de58c9c67
brownad <adrian-brown@mail.com>
2017-01-26 21:33:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -91,14 +91,13 @@ DoorBirdAccessory.prototype.setPowerOn = function(powerOn, callback) {
   }
 };
 
-DoorBirdAccessory.prototype.httpRequest = function(url, body, method, callback) {
+DoorBirdAccessory.prototype.httpRequest = function(url, method, callback) {
    request({
 	url: url,
 	method: method,
-	body: body,
 	
-	function(error, response, body) {
-		callback(error, response, body);
+	function(error, response ) {
+		callback(error, response);
 	},
    });
 };
